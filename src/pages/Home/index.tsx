33280f4294e61ca1db60db917987d64b8debc5bd
Thiago Brolly <thiagoex377@gmail.com>
2023-03-17 07:37:02 -0300

    feat: Layout & Header

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '../../components/Button';
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+    </div>
+  );
+}
