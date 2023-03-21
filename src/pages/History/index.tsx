7cff0ae004240fa5c31f3fee41b372bb7ccd86ce
Thiago Brolly <thiagoex377@gmail.com>
2023-03-21 15:21:02 -0300

    Finalizando página Home e History

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,56 @@
+import * as S from './styles';
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <S.HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <S.HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>
+                <S.Status statusColor="green">Concluído</S.Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </S.HistoryList>
+    </S.HistoryContainer>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,52 @@
-import { Button } from '../../components/Button';
+import { Play } from 'phosphor-react';
+import * as S from './styles';
 
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-    </div>
+    <S.HomeContainer>
+      <form action="">
+        <S.FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <S.TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+            <option value="Projeto 5" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <S.MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </S.FormContainer>
+
+        <S.CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <S.Separator>:</S.Separator>
+          <span>0</span>
+          <span>0</span>
+        </S.CountdownContainer>
+
+        <S.StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </S.StartCountdownButton>
+      </form>
+    </S.HomeContainer>
   );
 }
